312ca25bd434ad2b6d4117e15d250bd27115d504
alexbhart <alexhart@gmail.com>
2023-04-04 15:35:12 -0700

    worked on pretty stuff

--- logic.js
+++ logic.js
@@ -98,23 +98,42 @@ d3.json(geoJSON2).then(function(data) {
       style: styles,   
       // Binding a popup to each layer
       onEachFeature: function(feature, layer) {
-        layer.bindPopup("<strong>" + feature.properties.name + "</strong><br /><br />Name: " +
-          feature.properties.state_name + "<br /><br />Things: $" + feature.properties.name);
+        layer.bindPopup("<strong>" + feature.properties.County + "</strong><br /><br />Diabetes Percentage: " +
+        feature.properties.DiagnosedDiabetesPercentage + "<br /><br />Total Population: " + feature.properties.Pop2010);
           
       }
+
     }).addTo(map);
+    var legend = L.control({ position: "bottomleft" });
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += "<h4>Diabetes Percentage:</h4>";
+        div.innerHTML += `<i style="background: ${colors[6]}"></i><span> > 9.8 % </span><br>`;
+        div.innerHTML += `<i style="background: ${colors[5]}"></i><span></span><br>`;
+        div.innerHTML += `<i style="background: ${colors[4]}"></i><span></span><br>`;
+        div.innerHTML += `<i style="background: ${colors[3]}"></i><span></span><br>`;
+        div.innerHTML += `<i style="background: ${colors[2]}"></i><span></span><br>`;
+        div.innerHTML += `<i style="background: ${colors[1]}"></i><span></span><br>`;
+        div.innerHTML += `<i style="background: ${colors[0]}"></i><span> < 5.1 %</span><br>`;
+
+        return div;
+    };
+
+    legend.addTo(map);
 }
 )
+var colors = ['#feedde','#fdd0a2','#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04']
 function getColor(d) {
-    var colors = ['#feedde', '#fdd0a2', '#fdae6b', '#fd8d3c', '#e6550d', '#a63603']
+    
 
-    return d > 10.85 ? colors[0] :
-        d > 9.7 ? colors[1] :
-        d > 8.55 ? colors[2] :
+    return d > 10.85 ? colors[6] :
+        d > 9.7 ? colors[5] :
+        d > 8.55 ? colors[4] :
         d > 7.4 ? colors[3] :
-        d > 6.25 ? colors[4] :
-        d > 5.1 ? colors[5] :
-        colors[5];
+        d > 6.25 ? colors[2] :
+        d > 5.1 ? colors[1] :
+        colors[0];
 
 }
 function styles(feature) {
